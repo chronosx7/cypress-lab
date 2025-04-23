185831a983d04ff2032b97576ba19d6478fa9e54
Abel Jurado <abda7kado@hotmail.com>
2025-04-23 13:11:30 -0500

    Added api-based sign in function

---
+++ cypress/e2e/bstack-store/checkout.cy.ts
@@ -0,0 +1,18 @@
+import { api_sign_in_test_user } from "./pages/Sessions"
+
+describe('completes checkout', () => {
+    beforeEach(() => {
+        api_sign_in_test_user('demouser')
+        cy.visit('https://www.bstackdemo.com/')
+    })
+    it('logs in successfully', () => {
+        const logout_btn = '#signin'
+        const current_user_label = 'span.username'
+
+        cy.get(current_user_label).should('be.visible')
+        cy.get(logout_btn).scrollIntoView().should('be.visible').invoke('text').then((text) => {
+            expect(text).to.equal('Logout')
+        })
+    })
+})
+
---
+++ cypress/e2e/bstack-store/pages/Sessions.ts
@@ -0,0 +1,36 @@
+export function api_sign_in_test_user(username: string): void {
+    cy.session(`${username}-session`,() => {
+        cy.fixture('test_users').then((data) => {
+            const user = data.users[username]
+            cy.request({
+                method: 'POST',
+                url: 'https://www.bstackdemo.com/api/signin',
+                body: {
+                    userName: user.userName,
+                    password: user.password
+                }
+            }).then(() => {
+                window.sessionStorage.setItem('username', user.userName)
+            })
+        })
+    
+    })
+}
+/**
+ * TODOS - Checkout
+ * - Continue shopping button changes to checkout when there are items in cart
+ * - Checkout button navigates to /checkout
+ * - Shipping address form must be full before submitting a purchase
+ * - There's an order summary on the right. All added items are listed
+ * - List on the right shows number of items, price units of each item
+ * - Total is shown on the right at the end of the list
+ * - Total doesn't change between pages
+ * - Form submission navigates to /confirmation
+ * 
+ * - Confirmation page shows
+ *      - An order number
+ *      - Items list with total on the right
+ *      - Link to download a receipt
+ *      - Continue shopping button
+ * - Continue shopping button takes the user back to the store with an empty shopping cart
+ */
