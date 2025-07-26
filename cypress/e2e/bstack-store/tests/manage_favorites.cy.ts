693be63ee186af994ce2d4ee0fd42f6d4d552436
Abel Jurado <abda7kado@hotmail.com>
2025-07-26 17:19:06 -0500

    add toggle favortie method

--- cypress/e2e/bstack-store/tests/manage_favorites.cy.ts
+++ cypress/e2e/bstack-store/tests/manage_favorites.cy.ts
@@ -16,4 +16,12 @@ describe('Web store and shopping cart actions', () => {
         Products.toggle_favorite_product(1)
         Products.is_marked_favorite(1).should('equal', false)
     })
+    // Products makerd favorite in store are listed in favorites pages
+    // Products unmarked as favorite in store are not listed in favorites pages
+        // Favorites page list number of favorite products
+    // Products unmarked as favorite in favorites pages update instantly
+        // Favorites page list number of favorite products
+    // Products in favorites can be added to shopping cart
+    // Purchase flow can be completed starting in favorite page
 })
+
