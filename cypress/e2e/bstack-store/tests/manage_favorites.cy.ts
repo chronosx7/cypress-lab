c093eec3f568c65fc9c6233659bc850aee1e97f8
Abel Jurado <abda7kado@hotmail.com>
2025-08-18 18:18:38 -0500

    Implement add product to favorites

--- cypress/e2e/bstack-store/tests/manage_favorites.cy.ts
+++ cypress/e2e/bstack-store/tests/manage_favorites.cy.ts
@@ -2,7 +2,7 @@ import Products from "../pages/Products"
 import { ListProductData } from "../pages/Products"
 import { api_sign_in_test_user } from "../utils/Sessions"
 
-describe('Web store and shopping cart actions', () => {
+describe('Manage favorite products section', () => {
     before(() => {
         cy.clearAllSessionStorage()
         Cypress.session.clearAllSavedSessions()
@@ -11,13 +11,8 @@ describe('Web store and shopping cart actions', () => {
         api_sign_in_test_user('demouser')
         cy.visit('https://www.bstackdemo.com/')
     })
-    it.only('adds product to favorites', () => {
-        Products.toggle_favorite_product(1)
-        Products.is_marked_favorite(1).should('equal', true)
 
-        Products.toggle_favorite_product(1)
-        Products.is_marked_favorite(1).should('equal', false)
-
+    it('adds product to favorites', () => {
         Products.toggle_favorite_product(2)
         Products.is_marked_favorite(2).should('equal', true)
 
@@ -38,15 +33,19 @@ describe('Web store and shopping cart actions', () => {
         Products.get_displayed_products_count().should('equal', 2)
 
         // Products marked favorite in store are listed in favorites pages
-        // Products unmarked as favorite in store are not listed in favorites pages
         cy.get<ListProductData[]>('@fave_products').then((faves) => {
-            expect(faves[0].name).to.equal(Products.sync_read_listed_product_data(0).name)
-            expect(faves[1].name).to.equal(Products.sync_read_listed_product_data(1).name)
+            const fave_names = faves.map((fave) => { return fave.name })
+            expect(fave_names.includes(Products.sync_read_listed_product_data(0).name)).to.equal(true)
+            expect(fave_names.includes(Products.sync_read_listed_product_data(1).name)).to.equal(true)
         })
 
     })
     // Products unmarked as favorite in favorites pages update instantly
-        // Favorites page list number of favorite products
+    // Favorites page list number of favorite products
+    it.only('Favorite page updates immediately', () => {
+
+    })
+    
     // Products in favorites can be added to shopping cart
     // Purchase flow can be completed starting in favorite page
 })
