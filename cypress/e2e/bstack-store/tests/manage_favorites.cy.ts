8eedb0f5e55ba35fc742e254335feff7e8ada736
Abel Jurado <abda7kado@hotmail.com>
2025-08-18 18:27:09 -0500

    Validate favorites page updates immediately

--- cypress/e2e/bstack-store/tests/manage_favorites.cy.ts
+++ cypress/e2e/bstack-store/tests/manage_favorites.cy.ts
@@ -29,10 +29,8 @@ describe('Manage favorite products section', () => {
         cy.get('a#favourites').click()
         cy.url().should('contain', 'favourites')
 
-        // Favorites page lists number of favorite products
         Products.get_displayed_products_count().should('equal', 2)
 
-        // Products marked favorite in store are listed in favorites pages
         cy.get<ListProductData[]>('@fave_products').then((faves) => {
             const fave_names = faves.map((fave) => { return fave.name })
             expect(fave_names.includes(Products.sync_read_listed_product_data(0).name)).to.equal(true)
@@ -43,7 +41,20 @@ describe('Manage favorite products section', () => {
     // Products unmarked as favorite in favorites pages update instantly
     // Favorites page list number of favorite products
     it.only('Favorite page updates immediately', () => {
+        Products.toggle_favorite_product(2)
+        Products.is_marked_favorite(2).should('equal', true)
 
+        Products.toggle_favorite_product(3)
+        Products.is_marked_favorite(3).should('equal', true)
+
+        cy.get('a#favourites').click()
+        cy.url().should('contain', 'favourites')
+
+        Products.get_displayed_products_count().should('equal', 2)
+
+        Products.toggle_favorite_product(1)
+
+        Products.get_displayed_products_count().should('equal', 1)
     })
     
     // Products in favorites can be added to shopping cart
